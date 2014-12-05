9fb679bfa37b40dfde17d6d991e91f9107b2d7d4
George Matos <george.oliveira@quantaconsultoria.com>
2014-12-05 15:11:32 -0300

    Correção de erro na instalação do grub-customizer

--- prepareMySystem.sh
+++ prepareMySystem.sh
@@ -15,31 +15,33 @@
 . scripts/instalarEclipse.sh
 . scripts/instalarSublimeText3.sh
 . scripts/instalarNodejs.sh
-. scripts/mensPadrao.sh
 
 # Manutenção do SO
-. scripts/instalarGrubCustomizer
+. scripts/instalarGrubCustomizer.sh
+
+# Utils
+. scripts/mensPadrao.sh
 
 essentciais() {
   echo "Intalando aplicativos essenciais"
-# instalarGit
-# instalarCurl
-# instalarZsh
-# instalarVim
+  # instalarGit
+  # instalarCurl
+  # instalarZsh
+  # instalarVim
 }
 
 desktop() {
   echo "Intalando aplicativos de Desktop"
-# instalarIcones
-# instalarChrome
+  # instalarIcones
+  # instalarChrome
 }
 
 develop() {
   echo "Intalando aplicativos de desenvolvimento"
-# instalarSublimeText3
-# instalarJava7
-# instalarEclipse
-# instalarNodejs
+  # instalarSublimeText3
+  # instalarJava7
+  # instalarEclipse
+  # instalarNodejs
 }
 
 manutencao() {
